545a5163110ddeb29ff5688a077399315e4c7454
marceloangomes <marcelogomes723@gmail.com>
2022-03-29 18:58:34 -0300

    Inserir Banco de Dados

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,9 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace LanchesMac.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int Id { get; set; }
+        [StringLength(100,ErrorMessage ="O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage ="Campo obrigatório")]
+        [Display(Name ="Nome")]
         public string Nome { get; set; }
+        [StringLength(200,ErrorMessage ="O tamanho máximo é 200 caracteres")]
+        [Required(ErrorMessage ="Campo obrigatório")]
+        [Display(Name ="Descrição")]
+
         public string Descricao { get; set; }
 
         public List<Lanche> Lanches { get; set; }
--- Models/Lanche.cs
+++ Models/Lanche.cs
@@ -1,11 +1,35 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
 namespace LanchesMac.Models
 {
+    [Table("Lanches")]
     public class Lanche
     {
+        [Key]
         public int Id { get; set; }
+        [StringLength(80,MinimumLength = 10, ErrorMessage ="O {0} deve ter no minimo {1} e no máximo 80 caracteres")]
+        [Required(ErrorMessage ="Campo obrigatório")]
+        [Display(Name ="Nome do Lanche")]
+
         public string Nome { get; set; }
+        [MinLength(10,ErrorMessage ="O tamanho máximo é {0} caracteres")]
+        [MaxLength(200, ErrorMessage ="Descrição não pode exceder {0} caracteres")]
+        [Required(ErrorMessage ="Campo obrigatório")]
+        [Display(Name ="Descrição do Lanche")]
+
         public string DescricaoCurta { get; set; }
+        [MinLength(10,ErrorMessage ="O tamanho máximo é {0} caracteres")]
+        [MaxLength(200, ErrorMessage ="Descrição não pode exceder {0} caracteres")]
+        [Required(ErrorMessage ="Campo obrigatório")]
+        [Display(Name ="Descrição do Lanche")]
+
+
         public string DescricaoDetalhada { get; set; }
+        [Required(ErrorMessage ="Informe o Preço do lanche")]
+        [Display(Name ="Preço do Lanche")]
+        [Column(TypeName ="decimal(10,2)")]
+        [Range(1,999.99,ErrorMessage ="Valor do Lanche inválido")]
         public decimal Preco { get; set; }
         public string imagemUrl { get; set; }
         public string imagemThumbNailUrl { get; set; }
